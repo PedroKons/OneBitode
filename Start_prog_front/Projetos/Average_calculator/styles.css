e31c2be01d1c0096e2e8234e9f02a1ab7d327215
Pedro H K Goncalves <108807271+PedroKons@users.noreply.github.com>
2023-08-24 09:24:31 -0300

    Create styles.css

---
+++ Start_prog_front/Projetos/Average_calculator/styles.css
@@ -0,0 +1,40 @@
+:root {
+    --primary-color: red;
+    --secondary-color: #9fe677;
+    --tertiary-color: #022b4d;
+}
+
+body {
+    background: var(--tertiary-color);
+    font-size: 25px;
+    text-align: center;
+    
+}
+
+.title {
+    line-height: 1.5; 
+    letter-spacing: 2px;
+    word-spacing: 4px;
+    color: gainsboro;
+    text-transform: uppercase;
+    text-decoration: underline;
+    text-shadow: 2px 1px 2px gainsboro;
+    
+    
+}
+
+#container {
+    background: var(--secondary-color);
+    height: 300px;
+    width: 300px;
+    margin: 100px auto;
+    box-shadow: 3px 3px 3px gainsboro;
+    border-radius: 25px ;
+    
+}
+
+
+#totalAverage {
+    margin-top: 10px;
+}
+
