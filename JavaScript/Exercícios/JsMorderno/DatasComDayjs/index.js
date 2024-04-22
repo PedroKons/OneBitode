f39bf1c50c4074c81b645dcde13a19ce6146e8a0
Pedro H K Goncalves <108807271+PedroKons@users.noreply.github.com>
2024-04-22 08:23:12 -0300

    Update index.js

--- JavaScript/Exercícios/JsMorderno/DatasComDayjs/index.js
+++ JavaScript/Exercícios/JsMorderno/DatasComDayjs/index.js
@@ -1,1 +1,17 @@
+const dayjs = require('dayjs');
 
+function date(birth) {
+    const now = dayjs(); //data atual
+    const age = now.diff(birth, 'years'); // atribuido a idade
+    const nextBirthday = dayjs(birth).add(age + 1, 'year'); // atribuindo a data de proximo anieversario
+    const diffDaysBithday = nextBirthday.diff(now, 'day');
+    console.log(
+        'Idade: ' + age +
+        '\n' + nextBirthday.format('DD/MM/YYYY') +
+        '\n' + 'Quantos dias faltam para o proximo aniversário: ' + diffDaysBithday
+    );
+    
+}
+
+
+date('2002/08/14');
